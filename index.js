20a49073a31cde34f52d568235265759e05741b8
Aamir Nakhwa <aamirnakhwa@yahoo.com>
2023-07-12 01:53:33 +0500

    File name changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
